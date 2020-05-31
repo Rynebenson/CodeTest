456fdab74482306bbaa9331a8e8f05f2c791f635
rynebenson <rynebensont@gmail.com>
2020-05-31 17:44:11 -0400

    finished test

--- src/__tests__/basket.js
+++ src/__tests__/basket.js
@@ -6,7 +6,7 @@ import Basket from '../components/Basket';
 describe("Basket Component", () => {
     let wrapper,
         state,
-        dispatch = jest.fn(action => console.log(action));
+        dispatch = jest.fn();
 
     test("Renders empty basket message", () => {
         state = {
@@ -47,7 +47,7 @@ describe("Basket Component", () => {
             ],
             basket_visibility: false,
             whitelist: ["5ecf28c459d3781a2e99738e"],
-            sum: 0
+            sum: 1.78
         }
 
         wrapper = mount(
@@ -57,5 +57,39 @@ describe("Basket Component", () => {
         )
 
         expect(wrapper.find('div.item').length).toEqual(1)
+    })
+
+    test("Removing item from cart", () => {
+        state = {
+            loading: false,
+            zip: "91001",
+            basket: [
+                {
+                    "_id": "5ecf28c459d3781a2e99738e",
+                    "cheese": {
+                      "_id": "4647",
+                      "name": "Raschera",
+                      "country": "Italy",
+                      "price": 1.78
+                    },
+                    "zip": "91001",
+                    "percent_discount": 18,
+                    "out_of_stock": false
+                }
+            ],
+            basket_visibility: false,
+            whitelist: ["5ecf28c459d3781a2e99738e"],
+            sum: 1.78
+        }
+
+        wrapper = mount(
+            <Store.Provider value={[state, dispatch]}>
+                <Basket state={state} dispatch={dispatch} />
+            </Store.Provider>
+        )
+
+        wrapper.find('button.remove').simulate('click')
+
+        expect(dispatch).toHaveBeenCalled()
     })
 })
